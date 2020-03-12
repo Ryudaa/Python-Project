3ac5226bb1da984472dd72fb156a93c85bbc7f1b
Ryudaa <53323453+Ryudaa@users.noreply.github.com>
2020-03-12 19:13:49 +0700

    Update graph.py

--- Graph Theory/graph.py
+++ Graph Theory/graph.py
@@ -1,11 +1,19 @@
 from tabulate import tabulate
 from celline_graph import find_shortest_path as dist
 
+# Mencari jarak antar vertex pada suatu graf #
 ##################################################################################
+## -- Ini yang diedit -- ##
 
-vertex = ['v1','v2','v3','v4','v5','v6','v7','v8','v9','v10','v11','v12']
-header = ['X']+vertex
+# List vertex
+vertex1 = ['v1','v2','v3','v4','v5','v6','v7','v8','v9','v10','v11','v12']
 
+# Keterhubungan vertex
+# Format : 'vertex':['adjacent1','adjacent2']
+#
+# jika v1 adjacent dengan v2 maka ditulis dua kali (untuk setiap vertex)
+# 'v1':['v2'],
+# 'v2':['v1']
 graph1 = {'v1':['v2','v4'],
     'v2':['v1','v3','v7'],
     'v3':['v2','v10'],
@@ -20,9 +28,16 @@ graph1 = {'v1':['v2','v4'],
     'v12':['v10'],
     }
 
+# Bisa tambah graf, kalo graf yang sebelumnya sayang dibuang
+# jadi tulis lagi misal vertex2 = ['v1','v2',...], lalu vertex1 diganti vertex2
+# begitu juga dengan g
+vertex = vertex1
 g = graph1
+## -- Editnya sampai sini saja -- ##
 ##################################################################################
+## -- Sini ke bawah jangan di edit -- ##
 
+header = ['X']+vertex
 isi_table = []
 
 for v in vertex:
